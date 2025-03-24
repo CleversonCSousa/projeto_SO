c61e660ec26f1200f996edab216c2f1e28a01efb
Felipe Skubs Oliveira <162647564+Poueeerr@users.noreply.github.com>
2025-03-24 19:42:54 -0300

    Update App.tsx

--- frontendSO/src/App.tsx
+++ frontendSO/src/App.tsx
@@ -6,7 +6,6 @@ const App = () => {
       <div className={styles.info}>
         <h1>Simulador de Processos Primitivos</h1>
         <h2>Simulador desenvolvido para a disciplina de Sistemas Operacionais I</h2>
-        <h2>Proposta da disciplina aqui, colocar dps</h2>
         <h3 className={styles.text}>Docentes: Júlio Cezar Estrella e Sarita Mazzini Bruschi</h3>
         <h3 className={styles.text}>Alunos: Cleverson, Felipe, Yuri </h3>
       </div>
